b33b9ca7f0bc57e482a357fb0469b6c76a970ae1
Antony Lajes <tonymateuslajes@hotmail.com>
2023-09-08 12:26:16 -0300

    added new sha-1 keyDDD

---
+++ app/src/main/java/com/nomargin/cynema/ui/activity/create_profile_activity/CreateProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.nomargin.cynema.ui.activity.create_profile_activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nomargin.cynema.R
+
+class CreateProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_profile)
+    }
+}
