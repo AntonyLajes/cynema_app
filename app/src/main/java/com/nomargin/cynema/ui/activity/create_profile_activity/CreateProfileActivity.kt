aaa5790639f427afdbfedb6ad7a4ac92bfc2ef91
Antony Lajes <tonymateuslajes@hotmail.com>
2023-09-08 17:08:45 -0300

    refactored app logo to svg, created create profile activity

--- app/src/main/java/com/nomargin/cynema/ui/activity/create_profile_activity/CreateProfileActivity.kt
+++ app/src/main/java/com/nomargin/cynema/ui/activity/create_profile_activity/CreateProfileActivity.kt
@@ -2,9 +2,13 @@ package com.nomargin.cynema.ui.activity.create_profile_activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.nomargin.cynema.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class CreateProfileActivity : AppCompatActivity() {
+    private val createProfileViewModel: CreateProfileViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_profile)
---
+++ app/src/main/java/com/nomargin/cynema/ui/activity/create_profile_activity/CreateProfileViewModel.kt
@@ -0,0 +1,12 @@
+package com.nomargin.cynema.ui.activity.create_profile_activity
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CreateProfileViewModel @Inject constructor(): ViewModel() {
+
+
+
+}
