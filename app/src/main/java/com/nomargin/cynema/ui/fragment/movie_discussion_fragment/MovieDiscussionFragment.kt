9461f9575a00b8b68888d1b3c69c6cd1134a67e1
Antony Lajes <tonymateuslajes@hotmail.com>
2023-09-30 11:37:51 -0300

    start bottom sheet create post

--- app/src/main/java/com/nomargin/cynema/ui/fragment/movie_discussion_fragment/MovieDiscussionFragment.kt
+++ app/src/main/java/com/nomargin/cynema/ui/fragment/movie_discussion_fragment/MovieDiscussionFragment.kt
@@ -8,14 +8,11 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.nomargin.cynema.databinding.FragmentMovieDiscussionBinding
 import com.nomargin.cynema.ui.adapter.recycler_view.MovieDiscussionPostAdapter
-import com.nomargin.cynema.ui.sheet.NewPostSheetFragment
 
 class MovieDiscussionFragment : Fragment() {
 
     private var _binding: FragmentMovieDiscussionBinding? = null
     private val binding: FragmentMovieDiscussionBinding get() = _binding!!
-
-    private val TAG = "ModalBottomSheet"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,30 +23,14 @@ class MovieDiscussionFragment : Fragment() {
         return binding.root
     }
 
-    override fun onResume() {
-        super.onResume()
-        initClicks()
-    }
-
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
 
-    private fun initMovieDiscussionPostRecyclerView() {
-        binding.movieDiscussionPostRecyclerView.layoutManager =
-            LinearLayoutManager(requireContext())
+    private fun initMovieDiscussionPostRecyclerView(){
+        binding.movieDiscussionPostRecyclerView.layoutManager = LinearLayoutManager(requireContext())
         binding.movieDiscussionPostRecyclerView.adapter = MovieDiscussionPostAdapter()
-    }
-
-    private fun initClicks() {
-        binding.buttonNewPost.setOnClickListener {
-            val newPostSheetFragment = NewPostSheetFragment()
-            newPostSheetFragment.show(
-                requireActivity().supportFragmentManager,
-                TAG
-            )
-        }
     }
 
 }
